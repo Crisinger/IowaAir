2226cd5d5b630819a48c3f584e6cf48112f4a75b
crisi_000 <crisinger@uiowa.edu>
2017-03-23 12:51:23 -0500

    Initial commit

---
+++ src/com/util/ConnectionConfiguration.java
@@ -0,0 +1,7 @@
+package com.util;
+
+/**
+ * Created by crisi_000 on 3/21/2017.
+ */
+public class ConnectionConfiguration {
+}
---
+++ src/com/util/Servlet.java
@@ -0,0 +1,16 @@
+package com.util;
+
+import java.io.IOException;
+
+/**
+ * Created by crisi_000 on 3/21/2017.
+ */
+public class Servlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+}
